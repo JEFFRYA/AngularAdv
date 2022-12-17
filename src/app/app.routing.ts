4eb709d0f7d813bb7056832124d21bd41d089fef
JEFFRYA <jeffryaaads@hotmail.com>
2022-12-17 12:04:54 -0600

    Sección 4
    Estructurando Modulos, Componentes y Rutas (Router.Root y Router.Child)

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+
+// Routes
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+// Components
+import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NotPageFoundComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
